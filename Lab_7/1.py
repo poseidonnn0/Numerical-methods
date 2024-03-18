463583bee3e1bdcc2fd70264d883d44e9620e869
Vitaliy <79005626141@mail.ru>
2024-03-18 18:05:23 +0200

    7 lab

---
+++ Lab_7/1.py
@@ -0,0 +1,58 @@
+"""
+Лабораторная работа №7
+Студент ОНК «ИВТ» ВШ КНиИИ направления ПМиИ 3 курса
+Кондратьев Виталий
+Вариант 9
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def Ux0(x):
+    if x >= 0.5:
+        return 2
+    else:
+        return 4
+
+
+h = 0.1
+T = 0.001
+a = 1
+d = 1
+eps = 0.01
+p = int(a / h) + 1
+q = int(d / T) + 1
+U = [0] * p
+
+for i in range(p):
+    U[i] = [0] * q
+
+for i in range(0, p):
+    x = h * i
+    U[i][0] = Ux0(x)
+
+
+for j in range(0, q - 1):
+    for i in range(1, p - 1):
+        x = h * i
+        t = T * j
+        U[i][j + 1] = U[i][j] - T / h * U[i][j] * (U[i][j] - U[i - 1][j]) - eps ** 2 * T / (2 * h ** 3) * (
+                    U[i + 1][j] - U[i - 1][j]) * (U[i + 1][j] - 2 * U[i][j] + U[i - 1][j])
+        U[p - 1][j + 1] = U[i][j] - T / h * U[i][j] * (U[i][j] - U[i - 1][j])
+
+u, v = np.mgrid[0:p, 0:q]
+x = h * u
+y = T * v
+z = x - x
+for j in range(0, q):
+    for i in range(0, p):
+        z[i][j] = U[i][j]
+
+fig = plt.figure(figsize=plt.figaspect(0.5))
+axes = fig.add_subplot(1, 2, 1, projection='3d')
+axes.set_xlabel("x")
+axes.set_ylabel("t")
+axes.set_zlabel("U(x,t)")
+axes.plot_surface(x, y, z, cmap='plasma')
+plt.show()
